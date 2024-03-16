7ed31988f243422450f8cce3737ca9a3db3187d2
coderCleric <wescamphouse@gmail.com>
2024-03-16 14:23:29 -0600

    Add project files.

---
+++ JamHub/JamHub.cs
@@ -0,0 +1,26 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace JamHub
+{
+    public class JamHub : ModBehaviour
+    {
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(JamHub)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            newHorizons.LoadConfigs(this);
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            };
+        }
+    }
+
+}
