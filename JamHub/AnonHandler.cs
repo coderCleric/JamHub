976c6e200013a2e295a184ea2e502a771b0b295c
AnonymousStrangerOW <169866456+AnonymousStrangerOW@users.noreply.github.com>
2025-08-10 23:07:09 -0400

    new dialogue
    - ditzy updated dialogue
    - anon dialogue added (but they still don't work!)
    - coa dialogue added

---
+++ JamHub/AnonHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace JamHub
+{
+    public class AnonHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private GhostBrain anonBrain;
+        [SerializeField]
+        private CompoundLightSensor anonSensors;
+        [SerializeField]
+        private Transform triggerTransform;
+        [SerializeField]
+        private GameObject anonDialogue;
+        private bool isAnonPissed;
+
+        private void Start()
+        {
+            isAnonPissed = false;
+            anonBrain.enabled = true;
+            anonBrain.OnEnterDreamWorld();
+            anonBrain.EscalateThreatAwareness(GhostData.ThreatAwareness.SomeoneIsInHere);
+            anonBrain.WakeUp();
+            triggerTransform.localScale = Vector3.zero;
+        }
+
+        private void Update()
+        {
+            if (anonSensors.IsIlluminated() && !isAnonPissed)
+            {
+                isAnonPissed = true;
+                triggerTransform.localScale = Vector3.one;
+                Destroy(anonDialogue);
+            }
+        }
+    }
+}
