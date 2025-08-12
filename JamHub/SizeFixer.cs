e20dd203082eeb2816f17cb8f72be6e90fec126a
coderCleric <56094451+coderCleric@users.noreply.github.com>
2025-08-12 15:23:07 -0600

    Fixed goofy size stuff with player npc modders

---
+++ JamHub/SizeFixer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace JamHub
+{
+    /**
+     * Because the self NPC's like to get really big for no reason
+     */
+    public class SizeFixer : MonoBehaviour
+    {
+        [SerializeField] private float size = 1;
+        private void Update()
+        {
+            transform.localScale = new Vector3(size, size, size);
+            Destroy(this);
+        }
+    }
+}
